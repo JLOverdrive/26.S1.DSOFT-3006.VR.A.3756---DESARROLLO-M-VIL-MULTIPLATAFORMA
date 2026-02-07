8400e32579e038fa74ce5db105c4d9e3263be9bb
Josse Tesa <jlperez@estud.tesa.edu.ec>
2026-02-06 21:48:15 -0500

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'mi.primer.proyect',
+  appName: 'mi-primer-proyect',
+  webDir: 'dist'
+};
+
+export default config;
