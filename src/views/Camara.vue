a41a689a8ac20ba37931d973425c73f3c2cfd4a3
Josse <jlperez@estud.tesa.edu.ec>
2026-03-15 18:37:49 -0500

    actualizacion de la aplicacion con camara y gps

--- src/views/Camara.vue
+++ src/views/Camara.vue
@@ -10,6 +10,13 @@
             <div v-if="imageSrc">
                 <img :src="imageSrc" alt="Taken picture" />
             </div>
+
+            <ion-button @click="getLocation">Get Location</ion-button>
+            <div v-if="location">
+                <p>Lat: {{ location.latitude }}</p>
+                <p>Lng: {{ location.longitude }}</p>
+            </div>
+
         </ion-content>
     </ion-page>
 </template>
@@ -17,9 +24,12 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { Camera, CameraResultType } from '@capacitor/camera';
+import { Geolocation } from '@capacitor/geolocation';
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/vue';
+
 const imageSrc = ref<string | undefined>('');
-    const imageInfo = ref<any>(null);
+const imageInfo = ref<any>(null);
+const location = ref<any>(null);
 
 const takePicture = async () => {
   const image = await Camera.getPhoto({
@@ -30,6 +40,11 @@ const takePicture = async () => {
   imageInfo.value = image;
   const imageUrl = image.webPath;
   imageSrc.value = imageUrl;
+};
+
+const getLocation = async () => {
+  const coordinates = await Geolocation.getCurrentPosition();
+  location.value = coordinates.coords;
 };
 
 </script>
